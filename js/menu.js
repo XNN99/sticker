788699df9b4a1a937b868bc88a1cabbb4bc9c80b
carl.wang <carl.wang@logicmonitor.com>
2016-12-17 20:40:50 +0800

    crate blog

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 $(function(){
   var nb = $('#navbtn');
-  var n = $('#topnav nav');
+  var n = $('#topnav .navbar');
   
   $(window).on('resize', function(){
     
@@ -8,13 +8,13 @@ $(function(){
       // if the nav menu and nav button are both visible,
       // then the responsive nav transitioned from open to non-responsive, then back again.
       // re-hide the nav menu and remove the hidden class
-      $('#topnav nav').hide().removeAttr('class');
+      $('#topnav .navbar').hide().removeAttr('class');
     }
     if(nb.is(':hidden') && n.is(':hidden') && $(window).width() > 569) {
       // if the navigation menu and nav button are both hidden,
       // then the responsive nav is closed and the window resized larger than 560px.
       // just display the nav menu which will auto-hide at <560px width.
-      $('#topnav nav').show().addClass('keep-nav-closed');
+      $('#topnav  .navbar').show().addClass('keep-nav-closed');
     }
   }); 
   
@@ -24,7 +24,7 @@ $(function(){
   
   $('#navbtn').on('click', function(e){
     e.preventDefault();
-    $("#topnav nav").slideToggle(350);
+    $("#topnav  .navbar").slideToggle(350);
   });
   
 });
